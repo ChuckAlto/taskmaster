6ef401d7323d3f71e56d3a143720ad100af5932b
Your Name <chuckalto@gmail.com>
2022-06-22 17:57:20 -0400

    initial commit

---
+++ app/src/main/java/com/task/taskmaster/MainActivity.java
@@ -0,0 +1,16 @@
+package com.task.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+// just like out main() -- entry point
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
