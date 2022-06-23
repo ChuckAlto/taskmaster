3d064ef1611ef676494ad16273b3da7c02b7e9bc
Your Name <chuckalto@gmail.com>
2022-06-23 02:42:29 -0400

    lab 26 almost finished, need to add submitted label and screenshot.

---
+++ app/src/main/java/com/task/taskmaster/AllTasks.java
@@ -0,0 +1,15 @@
+package com.task.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_tasks);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
--- app/src/main/java/com/task/taskmaster/MainActivity.java
+++ app/src/main/java/com/task/taskmaster/MainActivity.java
@@ -2,7 +2,10 @@ package com.task.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 // just like out main() -- entry point
@@ -12,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button addTaskButton = MainActivity.this.findViewById(R.id.AddTaskButton);
+
+        addTaskButton.setOnClickListener(v -> {
+            Intent goToAddTaskIntent = new Intent(MainActivity.this, AddTask.class);
+            startActivity(goToAddTaskIntent);
+        });
+
+        Button allTasksButton = MainActivity.this.findViewById(R.id.AllTasksbutton);
+
+        allTasksButton.setOnClickListener(v -> {
+            Intent goToAllTasksIntent = new Intent(MainActivity.this, AllTasks.class);
+            startActivity(goToAllTasksIntent);
+        });
+//        addTaskButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
     }
+
+
 }
